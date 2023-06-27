869abcd62b82c50e4b4f078174d0a312ccbd9b79
wgj <ffos.wanggj@chinatelecom.cn>
2023-06-27 16:19:37 +0800

    Site updated: 2023-06-27 16:19:35

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "0124",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "05c73c393227447bbef9b11adeb263d2"
+  }
+}
