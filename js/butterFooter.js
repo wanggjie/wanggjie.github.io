6efa8f36996492cb55140a482d122185129f21ca
wgj <ffos.wanggj@chinatelecom.cn>
2023-07-09 03:20:43 +0800

    Site updated: 2023-07-09 03:20:41

--- js/butterFooter.js
+++ js/butterFooter.js
@@ -1,6 +1,6 @@
 // 动态心跳
 $(document).ready(function(e){
-  $('.copyright').html('©2020 - 2500 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> By 王菜牛-WP');
+  $('.copyright').html('©2021 - 2500 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> By 王菜牛-WP');
 })
 
 $(document).ready(function(e){
@@ -11,7 +11,7 @@ $(document).ready(function(e){
 function show_date_time(){
 $('.framework-info').html('菜牛驿站已经运行 <span id="span_dt_dt" style="color: #fff;"></span>');
 window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("1/01/2020 0:0:0");
+BirthDay=new Date("1/01/2021 0:0:0");
 today=new Date();
 timeold=(today.getTime()-BirthDay.getTime());
 sectimeold=timeold/1000
